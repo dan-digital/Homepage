ebfb2c354fa02995fcd17378a5b5373644543d6f
Daniel Yefet <daniel.yefet@gmail.com>
2014-12-17 20:26:39 +0000

    Removed admin js and moved main css

--- gruntfile.js
+++ gruntfile.js
@@ -4,44 +4,20 @@ module.exports = function(grunt) {
     	
         pkg: grunt.file.readJSON('package.json'),
 
-        concat: {
-        	admin: {
-        		src: ['www/js/admin/app.js', 'www/js/admin/**/*.js'],
-        		dest: 'www/js/build/dd-admin.js'
-        	}
-        },
-
-        uglify: {
-        	admin: {
-        		files: {
-        			'www/js/build/dd-admin.min.js': 'www/js/build/dd-admin.js'
-        		} 
-        	}
-        },
-
 		watch: {
 			options: {
-				livereload: true
+				livereload: true,
+				spawn: false
 			},
 		    app: {
-		    	files: ['application/**', 'system/**', 'www/index.php'],
-		    	options: {
-		    		spawn: false
-		    	}
+		    	files: ['index.html'],
 		    },
 		    styles: {
-		    	files: ['www/css/**/*.scss'],
+		    	files: ['css/**/*.scss'],
 		    	tasks: ['sass', 'autoprefixer'],
-		    	options: {
-		    		spawn: false
-		    	}
 		    },
 		    scripts: {
-		    	files: ['www/js/**/*.js'],
-		    	tasks: ['concat', 'uglify'],
-		    	options: {
-		    		spawn: false
-		    	}
+		    	files: ['js/**/*.js']
 		    }
 		},
 
@@ -49,27 +25,25 @@ module.exports = function(grunt) {
 			options: {
 				style: 'compressed'
 			},
-			admin: {
+			main: {
 				files: {
-					'www/css/build/admin.min.css' : 'www/css/admin/main.scss'
+					'css/build/main.min.css' : 'css/main.scss'
 				}
 			}
 		},
 
 		autoprefixer: {
 			all: {
-				src: 'www/css/build/**/*.css'
+				src: 'css/build/**/*.css'
 			}
 		}
 
     });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-autoprefixer');
 
-    grunt.registerTask('default', ['concat', 'uglify', 'sass', 'autoprefixer', 'watch']);
+    grunt.registerTask('default', ['sass', 'autoprefixer', 'watch']);
 
 };
